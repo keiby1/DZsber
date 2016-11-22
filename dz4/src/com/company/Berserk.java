1d1a04c880e8ece321bb58eb2eae5c15cad76d13
keiby1 <lushinaleksei@bk.ru>
2016-11-22 17:13:38 +0400

    Add files via upload

---
+++ dz4/src/com/company/Berserk.java
@@ -0,0 +1,52 @@
+package com.company;
+
+/**
+ * Created by lushi on 21.11.2016.
+ */
+public class Berserk implements Warrior {
+    String name, squad;
+    int health = 100;
+    int damage = 150;
+    @Override
+    public int attack() {
+        System.out.println("Получил урон " + damage);
+        return damage;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        System.out.println("Осталось здоровья: " + (health-damage));
+        health -= damage;
+    }
+
+    @Override
+    public boolean isAlive() {
+        if(health > 0)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public void setSquadName(String n) {
+        squad = n;
+    }
+    @Override
+    public String toString(){
+        return name+" " + this.getClass().getSimpleName().toString()+" " + squad;
+    }
+    @Override
+    public Berserk clone() {
+        Berserk w = new Berserk();
+        w.name = this.name;
+        return w;
+    }
+    @Override
+    public void setName(String n){
+        name = n;
+    }
+    @Override
+    public String getInfo(){
+        return health+ " " + damage;
+    }
+}
